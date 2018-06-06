e199a6146fc42a3ac63d56c5612bcb2f88a52a3c
Milos <zecevic.milos@hotmail.com>
2018-06-06 17:07:57 +0200

    react redux starter

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>App</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
